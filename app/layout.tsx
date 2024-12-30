1ffdc0ef59fdf620e2b6e8ee2aac995d618312ee
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-12-30 03:02:03 -0800

    Intial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Provider from "./providers";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Provider>
+
         {children}
+        </Provider>
       </body>
     </html>
   );
